b58264e6ee4c7096fc86425be3957f9eaa215720
SakshiK-218 <sakshikavya30@gmail.com>
2026-09-13 00:48:04 +0530

    Point frontend to deployed backend URL

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import './App.css';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://pharmacy-stock-manager.onrender.com/api';
 
 function App() {
   const [items, setItems] = useState([]);
